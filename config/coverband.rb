5e406e6f04abd0c897c109cc7efc8f8d1d97d19f
Dan Mayer <dan.mayer@stitchfix.com>
2019-11-07 12:51:38 -0700

    basic full flow sort of works

--- config/coverband.rb
+++ config/coverband.rb
@@ -1,10 +1,107 @@
 # frozen_string_literal: true
+require 'securerandom'
+
+module Coverband
+  module Adapters
+    ###
+    # Take Coverband data and store a merged coverage set to the Coverband service
+    ###
+    class Service < Base
+
+      attr_reader :coverband_url, :process_type, :runtime_env, :coverband_id
+
+      def initialize(coverband_url, opts = {})
+        super()
+        @coverband_url = coverband_url
+        @process_type = opts.fetch(:process_type){ 'unknown' }
+        @runtime_env = opts.fetch(:runtime_env){ Rails.env }
+        @coverband_id = opts.fetch(:coverband_id){ 'coverband-service/coverband_service_demo' }
+      end
+
+      def clear!
+        # TBD
+      end
+
+      def clear_file!(filename)
+        # TBD
+      end
+
+      # TODO: we should support nil to mean not supported
+      def size
+        0
+      end
+
+      def type=(type)
+        super
+      end
+
+      # TODO: no longer get by type just get full merged report
+      def coverage(local_type = nil, opts = {})
+        local_type ||= opts.key?(:override_type) ? opts[:override_type] : type
+        uri = URI("#{coverband_url}/api/coverage")
+        req = Net::HTTP::Post.new(uri, 'Content-Type' => 'application/json', 'Coverband-Token' => 'abcd')
+        req.body = {coverband_id: coverband_id, type: local_type}.to_json
+        res = Net::HTTP.start(uri.hostname, uri.port) do |http|
+          http.request(req)
+        end
+        coverage_data = JSON.parse(res.body)
+        puts "coverage data: "
+        puts coverage_data
+        coverage_data
+      rescue => err
+        puts "Coverband: Error while retrieving coverage #{err}"
+      end
+
+      def save_report(report)
+        # TODO: do we need dup
+        # TODO remove timestamps
+        return if report.empty?
+
+        data = expand_report(report.dup)
+        full_package = {
+          coverband_id: coverband_id,
+          collection_type: 'coverage_delta',
+          collection_data: {
+            tags: {
+              process_type: process_type,
+              app_loading: type == Coverband::EAGER_TYPE,
+              runtime_env: runtime_env
+            },
+            file_coverage: data
+          },
+        }
+        save_coverage(full_package)
+      end
+
+      def raw_store
+        raise 'not supported'
+      end
+
+      private
+
+      def save_coverage(data)
+        uri = URI("#{coverband_url}/api/collector")
+        req = Net::HTTP::Post.new(uri, 'Content-Type' => 'application/json', 'Coverband-Token' => 'abcd')
+        puts "sending #{data}"
+        req.body = {remote_uuid: SecureRandom.uuid, data: data}.to_json
+        res = Net::HTTP.start(uri.hostname, uri.port) do |http|
+          http.request(req)
+        end
+      rescue => err
+        puts "Coverband: Error while saving coverage #{err}"
+      end
+    end
+  end
+end
 
 Coverband.configure do |config|
   # toggle store type
   redis_url = ENV['REDIS_URL']
-  # config.store = Coverband::Adapters::MultiKeyRedisStore.new(Redis.new(url: redis_url))
-  config.store = Coverband::Adapters::HashRedisStore.new(Redis.new(url: redis_url))
+  # Normal Coverband Setup
+  # config.store = Coverband::Adapters::HashRedisStore.new(Redis.new(url: redis_url))
+  # Use The Test Service Adapter
+  coverband_service_url = ENV['COVERBAND_URL'] || 'http://127.0.0.1:3456'
+  config.store = Coverband::Adapters::Service.new(coverband_service_url)
   # in general I would leave the report time to the defaults,
   # but for the demo it is nice to update even faster
   config.background_reporting_sleep_seconds = 10
@@ -26,7 +123,8 @@ Coverband.configure do |config|
   # toggle on and off gem file details
   # config.gem_details = true
 
-  config.track_views = true
+  # TODO the initial service will not support tracking views
+  # config.track_views = false
 
   # ignores bin started to show in runtime only ;)
   # NOTE: that activerecord/* shows how to ignore gems
